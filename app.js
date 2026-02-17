776dcbd7d086dd092ed296b0accb107c65c8a2d8
Monterrosainfante <infantemonterrosa@gmail.com>
2026-02-17 11:03:02 -0600

    app.js

--- app.js
+++ app.js
@@ -86,4 +86,85 @@ if (assignStudentBtn) {
     alert("Student assigned successfully!");
   });
 }
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged
+  if (role === "teacher") {
+  loadTeacherClasses();
+}
+
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  serverTimestamp
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDshlcOrBShy1mhAXUoc5-Ppo3GqsbJHbs",
+  authDomain: "monterrosa-exam-system.firebaseapp.com",
+  projectId: "monterrosa-exam-system",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+    onAuthStateChanged(auth, async (user) => {
+
+  if (!user) return;
+
+  const userDoc = await getDoc(doc(db, "users", user.uid));
+
+  if (!userDoc.exists()) return;
+
+  const role = userDoc.data().role;
+
+  if (window.location.pathname.includes("index.html") || window.location.pathname.endsWith("/")) {
+
+    if (role === "admin") {
+      window.location.href = "admin.html";
+    } else if (role === "teacher") {
+      window.location.href = "teacher.html";
+    } else if (role === "student") {
+      window.location.href = "student.html";
+    }
+
+  }
+});
+import { collection, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+async function loadTeacherClasses() {
+
+  const list = document.getElementById("myClasses");
+  if (!list) return;
+
+  list.innerHTML = "";
+
+  const user = auth.currentUser;
+  if (!user) return;
+
+  const q = query(
+    collection(db, "classes"),
+    where("teacherId", "==", user.uid)
+  );
+
+  const snapshot = await getDocs(q);
+
+  snapshot.forEach((docSnap) => {
+    const data = docSnap.data();
+
+    const li = document.createElement("li");
+    li.textContent = data.name + " - Grade " + data.grade;
+
+    list.appendChild(li);
+  });
+}
+
 
